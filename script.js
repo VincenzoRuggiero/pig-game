57b57dd48a62599067b58f062f9924058b0dd85e
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-04-14 11:35:27 +0200

    Add dice functionality and score update

--- script.js
+++ script.js
@@ -3,9 +3,37 @@
 // Selecting elements
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
+let current0El = document.getElementById("current--0");
+let current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+// Storing current score
+let currentScore = 0;
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+
+// Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  // 1. Generating random dice-roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `img/dice-${dice}.png`;
+
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add the dice value to the score
+    currentScore += dice;
+    current0El.textContent = currentScore; // Change later
+
+    // Switch to next player
+  } else {
+  }
+});
